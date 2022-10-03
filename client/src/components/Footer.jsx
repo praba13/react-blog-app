3ce3daa9a9285ff116940495cab250ed1080f369
Praba <prama1303@gmail.com>
2022-10-03 22:58:45 +0200

    Navbar and Footer is ready

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import Logo from '../img/react-svgrepo-com.svg';
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer>
+      <img src={Logo} alt='LOGO-FOOTER' />
+      <span>
+        Made with <span style={{ color: 'red' }}>♥️</span> and <b>React.js</b>.
+      </span>
+    </footer>
   );
 };
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
-
+import { Link } from 'react-router-dom';
+import Logo from '../img/react-svgrepo-com.svg';
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className='navbar'>
+      <div className='container'>
+        <div className='logo'>
+          <Link to='/'>
+            <img src={Logo} alt='LOGO' />
+          </Link>
+        </div>
+        <div className='links'>
+          <Link className='link' to='/?cat=art'>
+            <h6>ART</h6>
+          </Link>
+          <Link className='link' to='/?cat=science'>
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className='link' to='/?cat=technology'>
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className='link' to='/?cat=cinema'>
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className='link' to='/?cat=design'>
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className='link' to='/?cat=food'>
+            <h6>FOOD</h6>
+          </Link>
+          <span>John</span>
+          <span>Logout</span>
+          <span className='write'>
+            <Link className='link' to='/write'>
+              Write
+            </Link>
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
